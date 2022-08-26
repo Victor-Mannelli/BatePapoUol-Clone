2cef5634c56a0f8a44a614f22c08c23ce06fb997
Victor Mannelli <imp.mannelli@gmail.com>
2022-08-26 17:03:54 -0300

    feat: servers messages are now loaded

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+
+let messages = document.querySelector('main ul')
+
 function hideEntrance(selector){
     let entrance = selector.parentNode;
     entrance.classList.add('hidden')
+}
+
+let request = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+request.then(addingMessages);
+
+
+
+function addingMessages(answer){
+    messages.innerHTML = ""
+
+    let messagesArray = answer.data;
+    for (i = 0; i < messagesArray.length; i++){
+        messages.innerHTML += 
+            `
+                <li> 
+                    <p> <span class="time"> ${messagesArray[i].time} </span> <span class="from"> ${messagesArray[i].from} </span> para <span class="from"> ${messagesArray[i].to} </span> : ${messagesArray[i].text} </p>
+                </li>
+            `
+    }
+    
+    
+    console.log(messages.innerHTML)
 }
