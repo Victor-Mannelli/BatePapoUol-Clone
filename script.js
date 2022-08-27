002ed96d63c0c2dec55ed1288247f31de54b25b4
Victor Mannelli <imp.mannelli@gmail.com>
2022-08-27 14:07:28 -0300

    feat: private messages are now pink and user stays online

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function hideEntrance(selector){
     let answer = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',loginName);
 
     answer.then(() => { setInterval(getMessages, 3000), entrance.classList.add('hidden') });
-    answer.catch((error) => alert(`Erro: ${error.response.status} - ${error.response.data}`));
+    answer.catch(() => alert(`Erro: Esse nome já está em uso ou não é válido!`));
 }
 getMessages();
 function getMessages(){
@@ -29,28 +29,47 @@ function addingMessages(answer){
     let messagesArray = answer.data;
     
     for (i = 0; i < messagesArray.length; i++){
-        if (messagesArray[i].type == "status"){
-            messages.innerHTML += 
-            `
-                <li class="gray"> 
-                    <p> <span class="time"> (${messagesArray[i].time}) </span> <span class="from"> ${messagesArray[i].from} </span> para <span class="from"> ${messagesArray[i].to}</span>: ${messagesArray[i].text} </p>
-                </li>
-            `
-        } else {
-            messages.innerHTML += 
-            `
-                <li> 
-                    <p> <span class="time"> (${messagesArray[i].time}) </span> <span class="from"> ${messagesArray[i].from} </span> para <span class="from"> ${messagesArray[i].to}</span>: ${messagesArray[i].text} </p>
-                </li>
-            `
+        switch(messagesArray[i].type){
+            case "status":
+                messages.innerHTML += 
+                    `
+                        <li class="gray"> 
+                            <p> <span class="time"> (${messagesArray[i].time}) </span> <span class="from"> ${messagesArray[i].from} </span> para <span class="from"> ${messagesArray[i].to}</span>: ${messagesArray[i].text} </p>
+                        </li>
+                    `
+            break;
+            case "private_message":
+                messages.innerHTML += 
+                    `
+                        <li class="pink"> 
+                            <p> <span class="time"> (${messagesArray[i].time}) </span> <span class="from"> ${messagesArray[i].from} </span> para <span class="from"> ${messagesArray[i].to}</span>: ${messagesArray[i].text} </p>
+                        </li>
+                    `
+            break;
+            default:
+                messages.innerHTML += 
+                    `
+                        <li> 
+                            <p> <span class="time"> (${messagesArray[i].time}) </span> <span class="from"> ${messagesArray[i].from} </span> para <span class="from"> ${messagesArray[i].to}</span>: ${messagesArray[i].text} </p>
+                        </li>
+                    `
         }
     }
     scrollToLastMessage()
 }
-
 function scrollToLastMessage(){
     let lists = document.querySelectorAll('li');
     let lastLI = lists[lists.length - 1]
     lastLI.scrollIntoView(true)
 }
 
+if(loginInput !== ""){
+    setInterval(stillOnline, 5000);
+}
+function stillOnline(){
+    const loginName = 
+    { 
+        name: loginInput.value
+    };
+    axios.post('https://mock-api.driven.com.br/api/v6/uol/status', loginName)
+}
